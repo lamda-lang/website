440114452998e5d78037ca959dd7d0174fc8e551
Konrad Kuehne <konrad.kuehne@rocketmail.com>
2013-06-12 22:44:19 +0200

    add simple css menu

--- src/clj/lamda/server/views.clj
+++ src/clj/lamda/server/views.clj
@@ -1,47 +1,108 @@
 (ns lamda.server.views
-  (:require
-    [hiccup
-      [page :refer [html5]]
-      [element :refer [javascript-tag]]
-     [page :refer [include-js]]]
-    [garden.core :refer [css]]))
+  (:use cssgen
+        [hiccup.core :only [html]]
+        [hiccup.page :only [html5 include-js]]
+        [hiccup.element :only [javascript-tag]]))
 
 (defn- run-clojurescript [path init]
   (list
    (include-js path)
    (javascript-tag init)))
 
-(def html-background-color "#e5e5e5")
-(def background-color "#1a1a1a")
-(def hover-background-color "#8C0806")
-
-
-(defn header-css []
-  [:#header
-   {:background background-color
-    :color "#ffffff"
-    :margin "0"
-    :text-align "center"
-    :font-size "large"
-    :height "150px"}
-   [:h1
-    {:padding "0px"
-     :margin "0"}]])
-
+(def header-height (px 150))
+(def default-font "Open Sans")
+(def header-font-size (pt 16))
+(def header-font-color (rgb :#F27E18))
+(def default-background-color (rgb :#242424))
 
 (defn body-css []
-  [:body :html
-    {:font "16px sans-serif"
-     :margin "0"
-     :padding "0"
-     :color "#000000"
-     :background html-background-color}])
+  ["body,html"
+   :font-family default-font
+   :font-size header-font-size
+   :margin 0])
 
+(defn header-css []
+  ["#header"
+   :background default-background-color
+   :color header-font-color
+   :margin 0
+   :text-align :center
+   [:h1
+    :margin 0]
+   [:p
+    :margin 0]])
+
+(defn nav-sub-css []
+  ["#nav > li.sub"
+   :position :relative
+   ["& ul"
+    :width (em 10)
+    :margin 0
+    :padding [(em 0.5) 0]
+    :list-style :none
+    :background default-background-color
+    :position :absolute
+    :top (em -1000)
+    ["& li"
+     :width (% 90)
+     :margin [0 :auto (em 0.3) :auto]
+     ["& a"
+      :height (% 100)
+      :display :block
+      :padding (em 0.4)
+      :color (rgb :#fff)
+      :font-weight :bold
+      :text-decoration :none
+      ["&:hover"
+       :background (rgb :#c60)
+       :text-decoration :none]]]]
+   ["&:hover ul"
+    :top (em 3)]])
+
+
+(defn nav-css []
+  ["#nav"
+   :margin 0
+   :padding [0 (em 1)]
+   :background default-background-color
+   :height (em 3)
+   :text-decoration :none
+   :list-style :none
+   ["& > li"
+    :float :left
+    :margin-right (em 0.5)
+    :padding [0 (em 1)]
+    ["& > a"
+     :float :left
+     :color (rgb :#c60)
+     :text-decoration :none
+     :line-height 3
+     :fint-weight :bold
+     :text-transform :uppercase
+     ["&:hover"
+      :color header-font-color
+      :text-decoration :none]]]])
 
 (defn default-css []
   (css
    (body-css)
-   (header-css)))
+   (header-css)
+   (nav-css)
+   (nav-sub-css)))
+
+
+(defn menu-html []
+  [:ul {:id "nav"}
+   [:li [:a {:href "#"} "Home"]]
+   [:li {:class "sub"} [:a {:href "#"} "Explore"]
+    [:ul
+     [:li [:a {:href "#"} "live repl"]]
+     [:li [:a {:href "#"} "Documentation"]]
+     [:li [:a {:href "#"} "Github"]]]]
+   [:li {:class "sub"} [:a {:href "#"} "About"]
+    [:ul
+     [:li [:a {:href "#"} "Us"]]
+     [:li [:a {:href "#"} "Other Work"]]]]])
 
 
 (defn index-page []
@@ -50,12 +111,9 @@
     [:title "Lamda Language"]
     [:style {:type "text/css"} (default-css)]]
    [:body
-    [:div {:id "header"}
-     [:h1 "The Lamda Language"]
-     [:p "Das ist doch Unsinn!"]]
-    ]))
-
-
-#_(run-clojurescript
-     "js/cljs.js"
-     "metis.repl.connect()")
+    [:div {:id "wrap"}
+     [:div {:id "header"}
+      [:h1 "The Lamda Language"]
+      [:p "Go functional!"]]
+     [:div {:id "menu"} (menu-html)]
+     ]]))
