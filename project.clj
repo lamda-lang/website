a626fe6b0631adeccb43b84093e030efe33ad723
Konrad Kuehne <konrad.kuehne@rocketmail.com>
2013-06-12 22:43:29 +0200

    change css library

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject lamda "0.1.0-SNAPSHOT"
-  :description "lamda-lang server n stuff"
+  :description "lamda-lang homepage n stuff"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -9,28 +9,11 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring-jetty-adapter "1.1.0"]
                  [com.cemerick/piggieback "0.0.4"]
-                 [garden "0.1.0-beta2"]
-                 [hiccup "1.0.2"]
-                 [compojure "1.1.5"]]
+                 [hiccup "1.0.3"]
+                 [compojure "1.1.5"]
+                 [cssgen "0.3.0-SNAPSHOT"]]
 
   :ring {:handler lamda.server.routes/app}
 
-  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
-
-  :plugins [[lein-cljsbuild "0.3.0"]
-            [lein-ring "0.8.3"]]
-
-  ;; cljsbuild tasks configuration
-  :cljsbuild
-  {
-   :repl-listen-port 9000
-   :repl-launch-commands
-   {"firefox" ["firefox"
-               :stdout ".repl-firefox-out"
-               :sterr ".repl-firefox-err"]
-    }
-   :builds
-   [{:source-paths ["src/cljs"]
-     :compiler
-     {:output-to "resources/public/js/cljs.js"
-      :optimizations :simple}}]})
+  :plugins [[lein-ring "0.8.3"]]
+)
